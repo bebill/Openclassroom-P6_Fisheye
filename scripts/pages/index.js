7159f3a37d013bf6ea22c4fe8e6c2070ba51dfe7
Ben Bld <bld.prom@gmail.com>
2023-06-15 22:21:15 +0200

    fix comments and indentation

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,28 @@
 async function fetchPhotographersJSON() {
-    //fetch data ;we use fetch to retrieve the data from the "photographers.json" file, and then we parse the JSON data using response.json(). Finally, we extract the photographers array from the parsed data and return it.
+    //retrieve the data from JSON file,
+    //parse the JSON data using response.json()
+    //extract the photographers array from the parsed data and return it.
     const response = await fetch('./data/photographers.json');
     const photographers = await response.json();
-    console.log(photographers); // affiche le tableau des photographers array dans la console
+    console.log(photographers); // return an array with photographers data in console.
     return photographers;
 }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await fetchPhotographersJSON();
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function init() {
+    //starts the process of loading the photographers' data and displaying it in the application
+    const { photographers } = await fetchPhotographersJSON();
+    displayData(photographers);
+}
+
+init();
+
