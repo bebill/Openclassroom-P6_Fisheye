de76485806d6aa8850eda2fedc41a9d629996a5f
Ben Bld <bld.prom@gmail.com>
2023-06-15 22:14:48 +0200

    chore : fetch photographer data from json

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,10 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function fetchPhotographersJSON() {
+    //fetch data ;we use fetch to retrieve the data from the "photographers.json" file, and then we parse the JSON data using response.json(). Finally, we extract the photographers array from the parsed data and return it.
+    const response = await fetch('./data/photographers.json');
+    const photographers = await response.json();
+    console.log(photographers); // affiche le tableau des photographers array dans la console
+    return photographers;
+}
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -38,7 +18,7 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const { photographers } = await fetchPhotographersJSON();
         displayData(photographers);
     }
     
