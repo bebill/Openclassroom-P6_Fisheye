da892268f1ca33e331529bab4fdfe8982bbb5a11
Ben Bld <bld.prom@gmail.com>
2023-06-20 20:26:17 +0200

    chore : update with header data display

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,38 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const urlParams = new URLSearchParams(window.location.search);
+const photographerId = urlParams.get('id');
+
+async function fetchPhotographerJSON() {
+  try {
+    const response = await fetch('./data/photographers.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function displayHeader() {
+  const dataHeader = await fetchPhotographerJSON();
+  const photographer = dataHeader.photographers.find(p => p.id === parseInt(photographerId));
+
+  const photographerTemplate = new PhotographerTemplate(photographer);
+  const infoArticle = photographerTemplate.createInfoHeader();
+  const buttonArticle = photographerTemplate.createButtonArticle();
+  const portraitArticle = photographerTemplate.createPortraitArticle();
+
+  const headerContainer = document.querySelector(".photograph-header");
+  headerContainer.appendChild(infoArticle);
+  headerContainer.appendChild(buttonArticle);
+  headerContainer.appendChild(portraitArticle);
+}
+
+
+
+async function init() {
+  await displayHeader();
+
+  // Perform additional initialization tasks here
+  // ...
+}
+
+init();
